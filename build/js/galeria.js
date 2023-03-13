f4d711ac6b55cf76b8debe4488594ea8b687ba47
Al <alejxndrods@gmail.com>
2023-03-12 21:35:57 -0500

    galeria terminada

--- build/js/galeria.js
+++ build/js/galeria.js
@@ -13,8 +13,46 @@ function crearGleria() {
         <source srcset="build/img/thumb/${i}.avif" type="image/avif">
         <source srcset="build/img/thumb/${i}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
-        `
+        `;
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
         galeria.appendChild(imagen);
     }
 
+}
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+        `;
+    //Crea el overlay con la imagen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body')
+        overlay.remove();
+    }
+
+
+    // Boton para cerrar la ventana modal o overlay
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X'
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body')
+        overlay.remove();
+
+    }
+
+    overlay.appendChild(cerrarModal);
+    // lo añade al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body')
 }
